ff5652b559d47e70f6119f4b11cee5dc982c878a
EdibleClover <36722535+EdibleClover@users.noreply.github.com>
2019-08-20 18:14:53 -0700

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -51,7 +51,6 @@ cd ~/Desktop/phpDebug
 
 ## install Composer, 
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === '48e3236262b34d30969dca3c37281b3b4bbe3221bda826ac6a9a62d6444cdb0dcd0615698a5cbe587c3f0fe57a54d8f5') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
 
@@ -161,4 +160,4 @@ echo -e "\
 touch ~/Desktop/phpDebug/mal.php
 
 ##Start vsCode   ##Fuck code for starting Firefox at the same Time
-vscode  ~/Desktop/phpDebug/mal.php
+code  ~/Desktop/phpDebug/mal.php
